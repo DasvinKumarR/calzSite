528433b7cfc801987e18ace579d0ae83680e54cc
DasvinKumarR <dasvinkumar2404@gmail.com>
2022-12-30 17:53:42 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+    let button = document.querySelector(".calBtn");
+    button.addEventListener("click", calculator);
+};
+
+function calculator() {
+    let number1 = parseInt(document.querySelector(".num1").value);
+    let number2 = parseInt(document.querySelector(".num2").value);
+    let result = document.querySelector(".resultValue");
+    let operat = document.querySelector("#operator");
+    let operatValue = operat.value;
+
+    console.log(operatValue);
+    if(operatValue === "addition") {
+        result.innerHTML = number1 + number2;
+    }
+    else if(operatValue === "subtraction") {
+        result.innerHTML = number1 - number2;
+    }
+    else if(operatValue === "multiply") {
+        result.innerHTML = number1 * number2;
+    }
+    else {
+        result.innerHTML = number1 / number2;
+    }
+}
